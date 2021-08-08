9ee637bcdce3c5e8db53c48d61e8f6a3a41b47cb
anushikghazaryan <anushghazaryan.23@gmail.com>
2021-08-08 15:27:28 +0400

    initial commit

---
+++ src/main/java/lmsProg/LmsApp.java
@@ -0,0 +1,11 @@
+package lmsProg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LmsApp.class, args);
+    }
+}
